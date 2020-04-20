286be90e936812aaaebb5e35c4cc16bf5f76e675
Aaron Mason <aaron.mason@thats-too-much.info>
2020-04-20 04:39:28 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # netatalk-tc
 Build scripts for building netatalk on Tinycore Linux
+
+# Required Dependencies for build
+* libgcrypt-dev
+* db-dev
---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+NEEDED=
+DESTDIR=$PWD/_install
+
+# Compiler flags
+
+case $(uname -m) in
+	x86) export CFLAGS="-march=i486 -mtune=i486 -Os -pipe"
+	     export CXXFLAGS="-march=i486 -mtune=i486 -Os -pipe"
+	     export LDFLAGS="-Wl,-O1"
+	     ;;
+	x86_64) export CFLAGS="-mtune=generic -Os -pipe"
+		export CXXFLAGS="-mtune=generic -Os -pipe"
+		export LDFLAGS="-Wl,-O1"
+		;;
+esac
+
+for i in compiletc libgcrypt-dev db-dev squashfs-tools
+do
+	if [ ! -f /usr/local/tce.installed/$i ]
+	then
+		NEEDED="$NEEDED $i"
+	fi
+done
+
+if [ "$NEEDED" ]
+then
+	tce-load -wi $NEEDED
+fi
+
+wget "https://prdownloads.sourceforge.net/netatalk/netatalk-3.1.12.tar.gz?download"
+
+tar -xvf netatalk-3.1.12.tar.gz
+cd netatalk-3.1.12
+
+./configure --prefix=/usr/local --disable-quota
+make
+make DESTDIR=$DESTDIR install
